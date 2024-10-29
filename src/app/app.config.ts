e6044fcd4f34d19ce7942a1c5d6c22915d9311a8
Juan Camilo Carreno Ramirez <jccarreno@unicauca.edu.co>
2024-10-29 11:22:15 -0500

    Actualizacion config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(withFetch())
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())]
 };
